974225ff638078851bb9a57451e172374ee1d34d
Divyansh Muley <divyansh.muley@graas.ai>
2025-07-18 22:23:58 +0530

    fixed issues for production again

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Divi/',
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   build: {
-    minify: true,
     rollupOptions: {
       output: {
         manualChunks: {
